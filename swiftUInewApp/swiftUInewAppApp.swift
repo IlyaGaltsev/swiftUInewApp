56dae8881328ea695d3916d259e0c5ea864895df
Ilya Galtsev <ilyandreev771@gmail.com>
2023-03-24 23:33:05 +0300

    Initial Commit

---
+++ swiftUInewApp/swiftUInewAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftUInewAppApp.swift
+//  swiftUInewApp
+//
+//  Created by Илья on 24.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct swiftUInewAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
